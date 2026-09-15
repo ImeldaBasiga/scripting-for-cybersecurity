7ff05315d9e03804e96967d60720deb498f2a01a
ImeldaBasiga <c00232690@setu.ie>
2026-09-15 18:34:35 +0000

    Completed Lab 1

---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+student = input("What is your student number? ")
+year = input("What year are you in? ")
+
+print("Hello, " + name + "!")
+print("Your student number is " + student)
+print("You are in year " + year)
